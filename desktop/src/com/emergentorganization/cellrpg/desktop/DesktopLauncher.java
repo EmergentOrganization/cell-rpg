b34199000abb92b7b63852db15bedc4cbf2f62a1
Brian Erikson <briankerikson@gmail.com>
2015-05-29 17:27:50 +0200

    Init with desktop test launch

---
+++ desktop/src/com/emergentorganization/cellrpg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.emergentorganization.cellrpg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.emergentorganization.cellrpg.CellRPG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CellRPG(), config);
+	}
+}

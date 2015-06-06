2955a9ff2a5d34b62c5f14df429c7cb564dbe43b
Orel Bitton <orelbitton@outlook.com>
2015-06-06 23:31:15 +0300

    Completed movement mechanics.

--- core/src/com/emergentorganization/cellrpg/components/player/PlayerInputComponent.java
+++ core/src/com/emergentorganization/cellrpg/components/player/PlayerInputComponent.java
@@ -3,6 +3,7 @@ package com.emergentorganization.cellrpg.components.player;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.emergentorganization.cellrpg.components.BaseComponent;
@@ -14,14 +15,17 @@ import com.emergentorganization.cellrpg.components.MovementComponent;
  */
 public class PlayerInputComponent extends BaseComponent {
 
+    // TODO: implement InputComponent to add AI support.
+
     private MovementComponent mc;
 
     private Camera camera; // Used to unproject screen coordinates for the mouse
     private Vector3 tmp = new Vector3();
 
-    private final float timeForWalking = 2000; // Time for mouse press/touch to be held for walking instead of shooting;
+    private final float timeForWalking = 200; // Time for mouse press/touch to be held for walking instead of shooting;
+
     private boolean lastFramePress = false; // If the left mouse button was pressed last frame
-    private long lastPress = TimeUtils.millis(); // Last press time in milliseconds
+    private long lastPress = 0; // Last press time in milliseconds
 
     public PlayerInputComponent(Camera camera){
         this.camera = camera;
@@ -34,24 +38,52 @@ public class PlayerInputComponent extends BaseComponent {
 
     @Override
     public void update(float deltaTime) {
+        // if left mouse button (touch down) is pressed in the current frame
         boolean framePress = Gdx.input.isButtonPressed(Input.Buttons.LEFT);
-        // listen for press events and broadcast a movement event
-        // if button is pressed in the current frame
+
+        long now = TimeUtils.millis();
+        long timePassed = now - lastPress;
+
+        // if the button wasn't pressed last frame
         if(framePress) {
-
-            // if button was released
+            if(!lastFramePress){
+                lastPress = now;
+            } else if(timePassed >= timeForWalking){
+                handleMovement();
+            }
+            // if the button was released
         }else if(!framePress && lastFramePress){
-
+            if(timePassed < timeForWalking) {
+                handleShooting();
+            }
+            lastPress = now;
         }
 
         lastFramePress = framePress;
     }
 
     private void handleMovement(){
-        System.out.println("Move");
+        // System.out.println("Move");
+
+        Vector3 v = getMouseCoords();
+
+        mc.moveTo(v.x, v.y);
     }
 
     private void handleShooting(){
-        System.out.println("Shoot");
+
+
+
+        // System.out.println("Shoot");
+    }
+
+    private Vector3 getMouseCoords(){
+        float x = Gdx.input.getX();
+        float y = Gdx.input.getY();
+
+        tmp.set(x, y, 0);
+        Vector3 v = camera.unproject(tmp);
+
+        return v;
     }
 }

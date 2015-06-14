969feecabc675889e422f502c5ec0dd80d8bbdeb
Brian Erikson <briankerikson@gmail.com>
2015-06-14 17:35:50 +0200

    map editor init. Everything is broken

---
+++ core/src/com/emergentorganization/cellrpg/tools/mapeditor/MapEditor.java
@@ -0,0 +1,26 @@
+package com.emergentorganization.cellrpg.tools.mapeditor;
+
+import com.emergentorganization.cellrpg.scenes.Scene;
+
+/**
+ * Created by BrianErikson on 6/14/2015.
+ */
+public class MapEditor extends Scene {
+
+    @Override
+    public void create() {
+        super.create();
+
+        
+    }
+
+    @Override
+    public void show() {
+        super.show();
+    }
+
+    @Override
+    public void hide() {
+
+    }
+}

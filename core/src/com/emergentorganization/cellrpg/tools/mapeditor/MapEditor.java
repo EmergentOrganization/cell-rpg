b817b4c1f66b994845f830089217af07249e0584
Brian Erikson <briankerikson@gmail.com>
2015-06-15 18:32:51 +0200

    modified layout of menuBar a bit

--- core/src/com/emergentorganization/cellrpg/tools/mapeditor/MapEditor.java
+++ core/src/com/emergentorganization/cellrpg/tools/mapeditor/MapEditor.java
@@ -2,8 +2,10 @@ package com.emergentorganization.cellrpg.tools.mapeditor;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Align;
 import com.emergentorganization.cellrpg.scenes.Scene;
 import com.kotcrab.vis.ui.VisUI;
@@ -14,8 +16,10 @@ import com.kotcrab.vis.ui.widget.*;
  */
 public class MapEditor extends Scene {
     VisList<String> entityList;
-    public static float MENU_BAR_HEIGHT = Gdx.graphics.getHeight() / 20f;
-    public static float LEFT_PANEL_WIDTH = (Gdx.graphics.getWidth() / 5f) - MENU_BAR_HEIGHT;
+    public static float LEFT_PANEL_HEIGHT = Gdx.graphics.getHeight();
+    public static float LEFT_PANEL_WIDTH = Gdx.graphics.getWidth() / 5f;
+    public static float MENU_BAR_HEIGHT = Gdx.graphics.getHeight() / 19f;
+    public static float MENU_BAR_WIDTH = Gdx.graphics.getWidth() - LEFT_PANEL_WIDTH;
 
     @Override
     public void create() {
@@ -31,18 +35,34 @@ public class MapEditor extends Scene {
     private void initMenuBar() {
         MenuBar menuBar = new MenuBar();
         Table table = menuBar.getTable();
-        table.setWidth(Gdx.graphics.getWidth());
+        table.setWidth(MENU_BAR_WIDTH);
         table.setHeight(MENU_BAR_HEIGHT);
-        table.setPosition(0f, Gdx.graphics.getHeight() - MENU_BAR_HEIGHT);
+        table.setPosition(LEFT_PANEL_WIDTH, Gdx.graphics.getHeight() - MENU_BAR_HEIGHT);
 
-        Menu translate = new Menu("Translate");
-        menuBar.addMenu(translate);
+        Menu modify = new Menu("Modify Object");
+        modify.setSkin(VisUI.getSkin());
+        menuBar.addMenu(modify);
 
-        Menu rotate = new Menu("Rotate");
-        menuBar.addMenu(rotate);
+        modify.addItem(new MenuItem("Translate", new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                System.out.println("Translate triggered");
+            }
+        }));
 
-        Menu scale = new Menu("Scale");
-        menuBar.addMenu(scale);
+        modify.addItem(new MenuItem("Rotate", new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                System.out.println("Rotate triggered");
+            }
+        }));
+
+        modify.addItem(new MenuItem("Scale", new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                System.out.println("Scale triggered");
+            }
+        }));
 
         getUiStage().addActor(menuBar.getTable());
     }
@@ -85,7 +105,7 @@ public class MapEditor extends Scene {
 
         VisWindow leftPane = new VisWindow("Assets");
         leftPane.setWidth(LEFT_PANEL_WIDTH);
-        leftPane.setHeight(Gdx.graphics.getHeight());
+        leftPane.setHeight(LEFT_PANEL_HEIGHT);
         leftPane.clearListeners(); // disable interaction on window
 
         Cell<VisScrollPane> scrollCell = leftPane.add(scrollPane);

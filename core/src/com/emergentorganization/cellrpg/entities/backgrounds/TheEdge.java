6b7ddab6c0bb277d8dba76983b1837798e21dfea
Brian Erikson <briankerikson@gmail.com>
2015-06-23 17:51:28 +0200

    background now selectable in editor. #28

--- core/src/com/emergentorganization/cellrpg/entities/backgrounds/TheEdge.java
+++ core/src/com/emergentorganization/cellrpg/entities/backgrounds/TheEdge.java
@@ -3,9 +3,14 @@ package com.emergentorganization.cellrpg.entities.backgrounds;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
+import com.emergentorganization.cellrpg.components.entity.PhysicsComponent;
 import com.emergentorganization.cellrpg.components.entity.SpriteComponent;
 import com.emergentorganization.cellrpg.entities.Entity;
 import com.emergentorganization.cellrpg.entities.ZIndex;
+import com.emergentorganization.cellrpg.physics.Tag;
+import org.dyn4j.dynamics.Body;
+import org.dyn4j.dynamics.BodyFixture;
+import org.dyn4j.geometry.Rectangle;
 
 /**
  * Created by BrianErikson on 6/12/2015.
@@ -13,6 +18,7 @@ import com.emergentorganization.cellrpg.entities.ZIndex;
 public class TheEdge extends Entity {
     public static final String ID = "the_edge";
     private final Texture texture;
+    private final SpriteComponent spriteComponent;
 
     /*
     This constructor is needed for MapEditor. Do not remove.
@@ -21,14 +27,33 @@ public class TheEdge extends Entity {
         super(ZIndex.BACKGROUND);
         texture = new Texture(Gdx.files.internal(ID + ".png"));
 
-        SpriteComponent spriteComponent = new SpriteComponent(texture);
+        spriteComponent = new SpriteComponent(texture);
         addComponent(spriteComponent);
+    }
+
+    @Override
+    public void added() {
+        super.added();
+
+        if (getScene().isEditor()) {
+            Vector2 size = new Vector2(spriteComponent.getSprite().getWidth() * getMovementComponent().getScale().x,
+                    spriteComponent.getSprite().getHeight() * getMovementComponent().getScale().y);
+            Rectangle rect = new Rectangle(size.x, size.y);
+            rect.translate(size.x / 2f, size.y /2f);
+            BodyFixture fixture = new BodyFixture(rect);
+            Body body = new Body();
+            body.addFixture(fixture);
+
+            PhysicsComponent physicsComponent = new PhysicsComponent(getScene().getWorld(), body, Tag.THE_EDGE);
+            physicsComponent.enableDebugRenderer(true);
+            addComponent(physicsComponent);
+        }
     }
 
     public TheEdge(Texture texture, Vector2 position) {
         super(ZIndex.BACKGROUND);
         this.texture = texture;
-        SpriteComponent spriteComponent = new SpriteComponent(texture);
+        spriteComponent = new SpriteComponent(texture);
         addComponent(spriteComponent);
 
         this.getMovementComponent().setWorldPosition(position);
--- core/src/com/emergentorganization/cellrpg/physics/Tag.java
+++ core/src/com/emergentorganization/cellrpg/physics/Tag.java
@@ -7,5 +7,6 @@ public enum Tag {
     PLAYER,
     BUILDING_LARGE_1,
     BUILDING_ROUND_1,
+    THE_EDGE,
     CHAR_CIV1_BLINKER
 }

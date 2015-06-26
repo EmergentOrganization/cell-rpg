2a86e5292fcdd2c5c05f17223280cdb8e3c00592
Brian Erikson <briankerikson@gmail.com>
2015-06-26 07:57:17 +0200

    readded code accidentally claimed in last commit for some reason

--- core/src/com/emergentorganization/cellrpg/tools/mapeditor/ui/EditorInputProcessor.java
+++ core/src/com/emergentorganization/cellrpg/tools/mapeditor/ui/EditorInputProcessor.java
@@ -90,12 +90,14 @@ public class EditorInputProcessor implements InputProcessor {
                 SpriteComponent sc = (SpriteComponent) entity.getFirstComponentByType(ComponentType.SPRITE);
                 SpriteComponent gc = (SpriteComponent) entity.getFirstComponentByType(ComponentType.GRAPHICS);
                 if (sc != null) {
-                    setMapTarget(sc.getSprite(), entity);
+                    boolean switched = switchMapTarget(sc.getSprite(), entity);
                     setDragOffset(screenCoords);
+                    if (switched) break;
                 }
                 else if (gc != null) {
-                    setMapTarget(gc.getSprite(), entity);
+                    boolean switched = switchMapTarget(gc.getSprite(), entity);
                     setDragOffset(screenCoords);
+                    if (switched) break;
                 }
                 else {
                     throw new RuntimeException("Cannot select a component with no render-able component");
@@ -104,6 +106,26 @@ public class EditorInputProcessor implements InputProcessor {
         }
 
         if (!foundBody) editor.setMapTarget(null);
+    }
+
+    /**
+     * Handles selecting map targets and switching to a different one if one is already selected in overlay instances
+     * @param sprite Sprite to switch to
+     * @param entity Entity to switch to
+     * @return whether or not a switch happened
+     */
+    private boolean switchMapTarget(Sprite sprite, Entity entity) {
+        if (editor.getMapTarget() != null) {
+            if (entity != editor.getMapTarget().target) {
+                setMapTarget(sprite, entity);
+                return true;
+            }
+            else return false;
+        }
+        else {
+            setMapTarget(sprite, entity);
+            return true;
+        }
     }
 
     private void onRightClick(Vector2 screenPos) {

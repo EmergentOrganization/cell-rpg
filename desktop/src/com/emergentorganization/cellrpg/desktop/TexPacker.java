4abfb8a96d66cfc23b65cb3b030ccd6e0316ae91
BrianErikson <briankerikson@gmail.com>
2015-09-17 12:37:35 +0200

    got packing to work. Porting code to use pack WIP

---
+++ desktop/src/com/emergentorganization/cellrpg/desktop/TexPacker.java
@@ -0,0 +1,17 @@
+package com.emergentorganization.cellrpg.desktop;
+
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
+/**
+ * Created by BrianErikson on 9/17/15.
+ */
+public class TexPacker {
+    public static String TEXTURE_DIR = "./android/assets/textures";
+
+    public static void main(String[] arg) {
+        TexturePacker.Settings settings = new TexturePacker.Settings();
+        settings.maxWidth = 4096;
+        settings.maxHeight = 4096;
+        TexturePacker.process(settings, TEXTURE_DIR + "/unpacked", TEXTURE_DIR, "TexturePack");
+    }
+}

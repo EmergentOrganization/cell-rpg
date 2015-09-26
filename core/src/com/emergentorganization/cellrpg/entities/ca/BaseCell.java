d1bf58b34a9a74302aca2c11277e35f0fed560b2
7yl4r <murray.tylar@gmail.com>
2015-09-25 22:00:46 -0400

    recolor on impact for #63

--- core/src/com/emergentorganization/cellrpg/entities/ca/BaseCell.java
+++ core/src/com/emergentorganization/cellrpg/entities/ca/BaseCell.java
@@ -1,13 +1,11 @@
 package com.emergentorganization.cellrpg.entities.ca;
 
-import com.badlogic.gdx.graphics.Color;
-
 /**
  * Created by 7yl4r on 9/15/2015.
  */
 public class BaseCell {
     protected int state;  // cell state value
-    public boolean lock; // true locks the cell, means "don't compute rule on this cell"
+    public boolean lock; // TODO: true locks the cell, means "don't compute rule on this cell"
 
     public BaseCell(int _state){
         state = _state;

d4905977d87921a573872870ceef0546ee725061
7yl4r <murray.tylar@gmail.com>
2015-10-03 21:34:04 -0400

    added gexf test to test default value setting (fails)

--- core/src/com/emergentorganization/cellrpg/entities/ca/CATestSuite.java
+++ core/src/com/emergentorganization/cellrpg/entities/ca/CATestSuite.java
@@ -1,9 +1,8 @@
 package com.emergentorganization.cellrpg.entities.ca;
 
-import com.emergentorganization.cellrpg.entities.ca.GeneticCellTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ GeneticCellTest.class} )
+@Suite.SuiteClasses({ GeneticCellTest.class, /*gexfTest.class*/} )
 public final class CATestSuite {}
---
+++ core/src/com/emergentorganization/cellrpg/entities/ca/gexfTest.java
@@ -0,0 +1,44 @@
+package com.emergentorganization.cellrpg.entities.ca;
+
+import it.uniroma1.dis.wsngroup.gexf4j.core.*;
+import it.uniroma1.dis.wsngroup.gexf4j.core.data.*;
+import it.uniroma1.dis.wsngroup.gexf4j.core.impl.GexfImpl;
+import it.uniroma1.dis.wsngroup.gexf4j.core.impl.data.AttributeListImpl;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import javax.xml.crypto.KeySelectorException;
+import java.util.Calendar;
+
+/**
+ * Created by 7yl4r on 10/3/2015.
+ */
+public class gexfTest {
+
+    @Test
+    public void testCellStateIsSetByConstructor() {
+        Gexf gexf = new GexfImpl();
+        Graph graph = gexf.getGraph();
+        graph.setDefaultEdgeType(EdgeType.UNDIRECTED).setMode(Mode.STATIC);
+
+        AttributeList attrList = new AttributeListImpl(AttributeClass.NODE);
+        graph.getAttributeLists().add(attrList);
+
+        final String DEFAULT_VALUE = "true";
+        Attribute attFrog = attrList.createAttribute("2", AttributeType.BOOLEAN, "frog")
+                .setDefaultValue(DEFAULT_VALUE);
+
+
+        Node gephi = graph.createNode("0");
+        gephi
+                .setLabel("Gephi");
+
+        // gephi should have default value for attFrog?
+        // but it doesn't, the following throws indexError, node has no attributes (default was not added)
+        AttributeValue att1 = graph.getNodes().get(0).getAttributeValues().get(1);
+        String v1 = att1.getValue();
+        String t1 = att1.getAttribute().getTitle();
+        System.out.println("attFrog value:" + v1 + " (" + t1 + ")");
+        Assert.assertEquals(v1, DEFAULT_VALUE);
+    }
+}

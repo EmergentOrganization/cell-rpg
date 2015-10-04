3e2c7716276029afc8a628f50557933152a0eeff
7yl4r <murray.tylar@gmail.com>
2015-10-03 21:12:40 -0400

    added some tests for alwaysOn node

--- core/src/com/emergentorganization/cellrpg/entities/ca/GeneticCell.java
+++ core/src/com/emergentorganization/cellrpg/entities/ca/GeneticCell.java
@@ -2,19 +2,18 @@ package com.emergentorganization.cellrpg.entities.ca;
 
 import com.emergentorganization.cellrpg.CellRpg;
 import it.uniroma1.dis.wsngroup.gexf4j.core.*;
-import it.uniroma1.dis.wsngroup.gexf4j.core.data.Attribute;
-import it.uniroma1.dis.wsngroup.gexf4j.core.data.AttributeClass;
-import it.uniroma1.dis.wsngroup.gexf4j.core.data.AttributeList;
-import it.uniroma1.dis.wsngroup.gexf4j.core.data.AttributeType;
+import it.uniroma1.dis.wsngroup.gexf4j.core.data.*;
 import it.uniroma1.dis.wsngroup.gexf4j.core.impl.GexfImpl;
 import it.uniroma1.dis.wsngroup.gexf4j.core.impl.StaxGraphWriter;
 import it.uniroma1.dis.wsngroup.gexf4j.core.impl.data.AttributeListImpl;
 
+import javax.xml.crypto.KeySelectorException;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * CA grid cell which has a digital gene regulatory network (DGRN) to represent it's genome,
@@ -23,67 +22,76 @@ import java.util.Calendar;
  * Created by 7yl4r on 9/25/2015.
  */
 public class GeneticCell extends BaseCell{
-    Gexf gexf;
+    private Gexf gexf;
+    private Graph graph;
+    // === SETUP NODE ATTRIBUTES ===
+    private static AttributeList attrList = new AttributeListImpl(AttributeClass.NODE);
+
+    private static Attribute attNodeType = attrList.createAttribute("0", AttributeType.STRING, "nodetype");
+    // node type defines how a node behaves when using the DGRN.
+
+    // === NODE TYPES ===  TODO: these could be in an enum, but there's no AttributeType.ENUM
+    // NOTE: this may not be the appropriate formulation, since these are not mutually exclusive types.
+    //           maybe better to have several booleans isGene, isTransFact, etc.
+
+    private static String nodeType_gene = "gene";
+    // node which expresses something (color, behavior). cell "outflow"
+    //     genes generally don't have outgoing connections (though they could if allowed to also be a TF)
+
+    private static String nodeType_transcriptionFactor = "transcriptionFactor";
+    // node that affects expression of another node.
+    //     DGRN "hidden" nodes. TFs have incoming & outgoing connections.
+
+    private static String nodeType_externalProtein = "externalProtein";
+    // node that has a value set from the cell environment
+    //     (onClick, neighborCount, alwaysOn). cell "inflow"
+    //     external proteins should have no inflows... though they could, if a TF was allowed to "fake" user input.
+
+    // === END NODE TYPES ===
+
+    private static Attribute attActive = attrList.createAttribute("activated", AttributeType.BOOLEAN, "activated")
+            .setDefaultValue("false");  // NOTE: default value doesn't seem to have indended effect?
+    // active state of a node defines wheter the node is
+
+    private static Attribute attTranscriptionFactorValue = attrList.createAttribute("tf_value", AttributeType.INTEGER,
+            "transcription factor value")
+            .setDefaultValue("1");
+    // === END SETUP NOTE ATTR ===
+
+    public GeneticCell(int _state, GeneticCell[] parents, int mutateLevel){
+        // builds cell network inherting from parent(s), mutated based on given level
+        this(_state);
+        // TODO: inherit + mutation
+    }
+
+    public GeneticCell(int _state, GeneticCell[] parents){
+        // builds cell network inherting from parent(s) (no mutations)
+        this(_state);
+        // TODO: inherit network from parents
+    }
 
     public GeneticCell(int _state){
+        // sets up cell with empty DGRN
         super(_state);
-        test();
+        initGraph();
     }
 
     public void initGraph(){
         gexf = new GexfImpl();
-        Calendar date = Calendar.getInstance();
-
         gexf.getMetadata()
-                .setLastModified(date.getTime())
+                .setLastModified(Calendar.getInstance().getTime())
                 .setCreator("Planiverse Bridge v" + CellRpg.VERSION)
                 .setDescription("Digital Gene Regulatory Network");
         gexf.setVisualization(false);
 
-    }
-
-    public void test() {
-        initGraph();
-        Graph graph = gexf.getGraph();
-        graph.setDefaultEdgeType(EdgeType.DIRECTED)
-                .setMode(Mode.STATIC);
-
-        // === SETUP NODE ATTRIBUTES ===
-        AttributeList attrList = new AttributeListImpl(AttributeClass.NODE);
+        graph = gexf.getGraph();
         graph.getAttributeLists().add(attrList);
 
-        Attribute attNodeType = attrList.createAttribute("0", AttributeType.STRING, "nodetype");
-        // node type defines how a node behaves when using the DGRN.
+        graph.setDefaultEdgeType(EdgeType.DIRECTED)
+                .setMode(Mode.STATIC);
+    }
 
-        // === NODE TYPES ===  TODO: these could be in an enum, but there's no AttributeType.ENUM
-        // NOTE: this may not be the appropriate formulation, since these are not mutually exclusive types.
-        //           maybe better to have several booleans isGene, isTransFact, etc.
-
-        String nodeType_gene = "gene";
-        // node which expresses something (color, behavior). cell "outflow"
-        //     genes generally don't have outgoing connections (though they could if allowed to also be a TF)
-
-        String nodeType_transcriptionFactor = "transcriptionFactor";
-        // node that affects expression of another node.
-        //     DGRN "hidden" nodes. TFs have incoming & outgoing connections.
-
-        String nodeType_externalProtein = "externalProtein";
-        // node that has a value set from the cell environment
-        //     (onClick, neighborCount, alwaysOn). cell "inflow"
-        //     external proteins should have no inflows... though they could, if a TF was allowed to "fake" user input.
-
-        // === END NODE TYPES ===
-
-        Attribute attActive = attrList.createAttribute("1", AttributeType.BOOLEAN, "activated")
-                .setDefaultValue("false");
-        // active state of a node defines wheter the node is
-
-        Attribute attTranscriptionFactorValue = attrList.createAttribute("2", AttributeType.INTEGER, "tf_value")
-                .setDefaultValue("1");
-        // === END SETUP NOTE ATTR ===
-
-
-
+    public void setGraphToDefault() {
         // Create test graph of shape:
         //   (on) -> (TF1) -> (colorAdd)
         //  (onClick) -^
@@ -93,19 +101,22 @@ public class GeneticCell extends BaseCell{
                 .setLabel("alwaysOn")
                 .getAttributeValues()
                 .addValue(attNodeType, nodeType_externalProtein)
-                .addValue(attActive, "true");
+                .addValue(attActive, "true")
+                .addValue(attTranscriptionFactorValue, "1");
 
         Node TF1 = graph.createNode("TF1");
         TF1
                 .setLabel("TF1")
                 .getAttributeValues()
-                .addValue(attNodeType, nodeType_transcriptionFactor);
+                .addValue(attNodeType, nodeType_transcriptionFactor)
+                .addValue(attTranscriptionFactorValue, "1");
 
         Node colorAdd1 = graph.createNode("colorAdd(50,50,50)");
         colorAdd1
                 .setLabel("colorAdd(50,50,50)")
                 .getAttributeValues()
-                .addValue(attNodeType, nodeType_gene);
+                .addValue(attNodeType, nodeType_gene)
+                .addValue(attTranscriptionFactorValue, "1");
 
 
         alwaysOn.connectTo("0", TF1);
@@ -123,5 +134,30 @@ public class GeneticCell extends BaseCell{
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    protected static String getNodeAttributeValue(Node node, String attributeId) throws KeySelectorException{
+        // gets value from attribute with given Id from given node
+        // throws KeySelectorException if node not found
+        // TODO: this should should really be handled within the gexf4j library.
+        for (AttributeValue attr : node.getAttributeValues()){
+            if (attr.getAttribute().getId() == attributeId){
+                return attr.getValue();
+            }
+        } // else
+        throw new KeySelectorException("attribute '" + attributeId + "' not found!");
+    }
+
+    protected Node getNode(String nodeId) throws KeySelectorException{
+        // returns node found using given nodeId
+        // throws KeySelectorException if node not found
+        // TODO: this should should really be handled within the gexf4j library.
+        List<Node> nodes = graph.getNodes();
+        for( Node node : nodes){
+            if (node.getId() == nodeId){
+                return node;
+            }
+        } // else
+        throw new KeySelectorException("node w/ id '" + nodeId + "' not found!");
     }
 }
--- core/src/com/emergentorganization/cellrpg/entities/ca/GeneticCellTest.java
+++ core/src/com/emergentorganization/cellrpg/entities/ca/GeneticCellTest.java
@@ -3,6 +3,8 @@ package com.emergentorganization.cellrpg.entities.ca;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import javax.xml.crypto.KeySelectorException;
+
 /**
  * Created by 7yl4r on 10/3/2015.
  */
@@ -20,5 +22,22 @@ public class GeneticCellTest {
         int INIT_STATE = 0;
         GeneticCell testCell = new GeneticCell(INIT_STATE);
         Assert.assertEquals("initial state should be set to " + INIT_STATE, INIT_STATE, testCell.getState());
+    }
+
+    @Test
+    public void testDefaultGraphHasAlwaysOnNode() throws KeySelectorException {
+        GeneticCell testCell = new GeneticCell(0);
+        testCell.setGraphToDefault();
+        testCell.getNode("alwaysOn");
+    }
+
+    @Test
+    public void testDefaultGraphAlwaysOnNodeIsActive() throws KeySelectorException, Exception {
+        GeneticCell testCell = new GeneticCell(0);
+        testCell.setGraphToDefault();
+        int TF = Integer.parseInt(GeneticCell.getNodeAttributeValue(testCell.getNode("alwaysOn"), "tf_value"));
+        if (TF < 1){
+            throw new Exception("alwaysOn.TF should be > 1, found:" + TF);
+        }
     }
 }

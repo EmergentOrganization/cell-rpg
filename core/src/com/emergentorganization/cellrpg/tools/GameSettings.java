3e8a0740904bd956eea3bf98f07f1898074ac06e
7yl4r <murray.tylar@gmail.com>
2016-01-03 15:53:55 -0500

    very complex and smart fix for nullPointerException

--- core/src/com/emergentorganization/cellrpg/tools/GameSettings.java
+++ core/src/com/emergentorganization/cellrpg/tools/GameSettings.java
@@ -9,15 +9,14 @@ import com.badlogic.gdx.Preferences;
 public class GameSettings {
     public static String KEY_DEV_DEVMODE = "DevMode"; // Naming syntax is TYPE_CATEGORY_NAME
 //    public static String KEY_INPUT_METHOD = "InputMethod";  // TODO: replaced w/ the next two:
-    public static String KEY_MOVEMENT_CONTROL_METHOD;
-    public static String KEY_WEAPON_CONTROL_METHOD;
+    public static String KEY_MOVEMENT_CONTROL_METHOD = "movement-ctrl-method";
+    public static String KEY_WEAPON_CONTROL_METHOD = "weapon-ctrl-method";
     public static String KEY_CAM_EDGE_MARGIN = "Cam_EdgeMargin";
     public static String KEY_CAM_LEAD = "Cam_Lead";
     public static String KEY_CAM_NEARNESS_CUTOFF = "Cam_NearnessCutoff";
     public static String USER_ID = "user_id";
     private static String PREFS_FILE = "com.emergentorganization.cellrpg.configuration";
     private boolean devModeEnabled = false;
-    private Preferences prefs;
 
     public static Preferences getPreferences() {
         return Gdx.app.getPreferences(PREFS_FILE);

834f78b856ac0739f758f3fe6d60ebcc07968475
Brian Erikson <briankerikson@gmail.com>
2016-01-11 10:01:09 +0100

    added epsilon const for color range

--- android/assets/resources/shaders/tron/Tron_Frag_ColorMask.glsl
+++ android/assets/resources/shaders/tron/Tron_Frag_ColorMask.glsl
@@ -6,10 +6,12 @@ precision mediump float;
 uniform sampler2D u_texture;
 uniform vec3 u_maskColor;
 varying vec2 v_textCoord;
+const float EPSILON = 0.1f;
 
 void main() {
     vec4 pixel = texture2D(u_texture,v_textCoord);
-    if (pixel.rgb == u_maskColor) {
+    vec3 diff = abs(pixel.rgb - u_maskColor);
+    if (all(lessThan(diff, vec3(EPSILON)))) {
         gl_FragColor = pixel;
     }
     else {

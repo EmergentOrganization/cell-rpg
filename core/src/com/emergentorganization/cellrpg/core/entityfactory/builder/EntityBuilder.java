b3fd610af64c7ec012dd4e49393ad1bca95379c4
Brian Erikson <briankerikson@gmail.com>
2016-01-11 21:33:13 +0100

    nearly refactored EntityBuilder. Dealing with some bugs

---
+++ core/src/com/emergentorganization/cellrpg/core/entityfactory/builder/EntityBuilder.java
@@ -0,0 +1,138 @@
+package com.emergentorganization.cellrpg.core.entityfactory.builder;
+
+import com.artemis.Archetype;
+import com.artemis.Entity;
+import com.artemis.World;
+import com.artemis.managers.TagManager;
+import com.badlogic.gdx.math.Vector2;
+import com.emergentorganization.cellrpg.components.*;
+import com.emergentorganization.cellrpg.core.entityfactory.EntityFactory;
+import com.emergentorganization.cellrpg.core.entityfactory.builder.componentbuilder.BuilderComparator;
+import com.emergentorganization.cellrpg.core.entityfactory.builder.componentbuilder.IComponentBuilder;
+
+import java.util.*;
+
+/**
+ * Created by brian on 11/21/15.
+ */
+public class EntityBuilder {
+    // ==================================================================
+    // ================= ENTITY COMPONENT PROPERTIES ====================
+    // === these are the properties to be added to the entity which   ===
+    // === should be modified using their homonym-ous setter methods. ===
+    // === Default values should be set here.                         ===
+    // ==================================================================
+    // REQUIRED
+    private final World world;
+    private final Vector2 position;
+    private final String entityId;
+    private final Archetype archetype;
+    private final String friendlyName;
+
+    // MISC
+    private ArrayList<IComponentBuilder> builders;
+    private String tag = null;
+    private float scale = EntityFactory.SCALE_WORLD_TO_BOX;
+    private float angleDeg = 0f;
+    private Vector2 velocity = new Vector2();
+
+    // ==================================================================
+    // ==================================================================
+
+    public EntityBuilder(World world, Archetype archetype, String friendlyName, String entityId, Vector2 position) {
+        this.world = world;
+        this.archetype = archetype;
+        this.position = position;
+        this.entityId = entityId;
+        this.friendlyName = friendlyName;
+        this.builders = new ArrayList<IComponentBuilder>();
+    }
+
+
+    // ==================================================================
+    // ====================== BUILD METHOD ==============================
+    // ===  Called at the end of the builder chain, this method       ===
+    // ===  actually builds and returns the entity using the          ===
+    // ===  properties specified using the component builder classes. ===
+    // ==================================================================
+
+    public Entity build() {
+        // called at the end of the builder chain, this actually builds and returns the entity
+        // using the properties specified using the setter methods.
+        Collections.sort(builders, new BuilderComparator());
+        Entity entity = world.createEntity(archetype);
+
+        buildName(entity);
+
+        if (tag != null) {
+            world.getSystem(TagManager.class).register(tag, entity);
+        }
+
+        entity.getComponent(Position.class).position.set(position);
+        Scale sc = entity.getComponent(Scale.class);
+        if (sc != null) {
+            sc.scale = scale; // player ends up being 1 meter in size
+        }
+
+        Rotation rc = entity.getComponent(Rotation.class);
+        if (rc != null) {
+            rc.angle = angleDeg;
+        }
+
+        Velocity vc = entity.getComponent(Velocity.class);
+        if (vc != null) {
+            vc.velocity.set(velocity);
+        }
+
+        for (IComponentBuilder builder : builders) {
+            builder.build(entity);
+        }
+
+        return entity;
+    }
+
+    private void buildName(Entity entity){
+        Name name = entity.getComponent(Name.class);
+        name.friendlyName = friendlyName;
+        name.internalID = entityId;
+    }
+
+    public EntityBuilder addBuilder(IComponentBuilder builder) {
+        this.builders.add(builder);
+        return this;
+    }
+
+    public EntityBuilder setBuilders(Collection<IComponentBuilder> builders) {
+        this.builders.clear();
+        this.builders.addAll(builders);
+        return this;
+    }
+
+    public EntityBuilder tag(String tag) {
+        this.tag = tag;
+        return this;
+    }
+
+    /**
+     * Defaults to SCALE_WORLD_TO_BOX
+     */
+    public EntityBuilder scale(float scale) {
+        this.scale = scale;
+        return this;
+    }
+
+    public EntityBuilder angle(float angleDeg) {
+        this.angleDeg = angleDeg;
+        return this;
+    }
+
+    public EntityBuilder velocity(Vector2 vel) {
+        this.velocity.set(vel);
+        return this;
+    }
+
+    public EntityBuilder velocity(float speed, Vector2 dir) {
+        this.velocity.set(dir).scl(speed);
+        return this;
+    }
+}

6f599f22c6b3d59aa7c8745b3ea75c30d5efd6b2
7yl4r <murray.tylar@gmail.com>
2016-01-17 03:10:49 -0500

    rename namespace

---
+++ core/src/io/github/emergentorganization/cellrpg/input/scripted/Script.java
@@ -0,0 +1,79 @@
+//package com.emergentorganization.cellrpg.input.scripted;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//
+///**
+// * Created by OrelBitton on 09/06/2015.
+// */
+//public class Script {
+//
+//    private ArrayList<ScriptAction> actions;
+//    private int i = 0;
+//    private boolean loop = true;
+//
+//    private boolean curScriptStarted = false;
+//
+//    protected ScriptedInputComponent input;
+//
+//    public Script(){
+//        this.actions = new ArrayList<ScriptAction>();
+//    }
+//
+//    public Script(ScriptAction[] actions){
+//        this.actions = new ArrayList<ScriptAction>(Arrays.asList(actions));
+//    }
+//
+//    public Script(ArrayList<ScriptAction> actions){
+//        this.actions = actions;
+//    }
+//
+//    // used to resolve dependencies
+//    public void added(ScriptedInputComponent input){
+//        for(ScriptAction sa : actions)
+//            sa.init(input);
+//    }
+//
+//    public void restart(){
+//        i = 0;
+//    }
+//
+//    public void run(){
+//        if(actions.isEmpty())
+//            return;
+//
+//        ScriptAction current = actions.get(i);
+//
+//        if(!curScriptStarted)
+//        {
+//            curScriptStarted = true;
+//            current.start();
+//        }
+//
+//        if(!current.completed())
+//        {
+//            current.run();
+//        }
+//        else {
+//            i++;
+//            curScriptStarted = false;
+//        }
+//    }
+//
+//    public void addAction(ScriptAction action) {
+//        actions.add(action);
+//    }
+//
+//    public boolean completed(){
+//        return i == actions.size();
+//    }
+//
+//    public boolean isLoop() {
+//        return loop;
+//    }
+//
+//    public void setLoop(boolean loop) {
+//        this.loop = loop;
+//    }
+//
+//}
---
+++ core/src/io/github/emergentorganization/cellrpg/input/scripted/actions/MoveToAction.java
@@ -0,0 +1,51 @@
+//package com.emergentorganization.cellrpg.input.scripted.actions;
+//
+//import com.emergentorganization.cellrpg.components.entity.MovementComponent;
+//import com.emergentorganization.cellrpg.components.entity.input.scripted.ScriptAction;
+//import com.emergentorganization.cellrpg.components.entity.input.scripted.ScriptedInputComponent;
+//import com.badlogic.gdx.math.Vector2;
+//
+///**
+// * Created by OrelBitton on 10/06/2015.
+// */
+//public class MoveToAction extends ScriptAction {
+//
+//    private MovementComponent mc;
+//    private float x, y;
+//    private boolean completed;
+//
+//    public MoveToAction(float x, float y){
+//        this.x = x;
+//        this.y = y;
+//    }
+//
+//    @Override
+//    public void init(ScriptedInputComponent input) {
+//        mc = input.getMovementComponent();
+//    }
+//
+//    @Override
+//    public void start() {
+//        //Vector2 pos = mc.getWorldPosition().add(x, y);
+//        //mc.setDest(pos); TODO: Removed destination triggers in movement component to preserve composition. re-implement
+//
+//        completed = false;
+//    }
+//
+//    @Override
+//    public void run() {
+//        //completed = mc.getDest() == null; TODO: Removed destination triggers in movement component to preserve composition. re-implement
+//    }
+//
+//    @Override
+//    public boolean completed() {
+//        if (completed) {
+//            // this needs to be done inside of the movement component when it reaches destination
+//            // we are not currently doing it cause it makes the player movement not smooth
+//            mc.setVelocity(Vector2.Zero);
+//        }
+//
+//        return completed;
+//    }
+//
+//}

fd626aaf577b43249b142c9c61acb6f432c608e2
7yl4r <murray.tylar@gmail.com>
2016-02-18 15:29:44 -0500

    expand decayCA color palattes a bit for more graceful fades

--- core/src/io/github/emergentorganization/cellrpg/systems/CASystems/CARenderSystem/CellRenderers/DecayCellRenderer.java
+++ core/src/io/github/emergentorganization/cellrpg/systems/CASystems/CARenderSystem/CellRenderers/DecayCellRenderer.java
@@ -19,23 +19,28 @@ public class DecayCellRenderer implements iCellRenderer {
             new Color(1f, 1f, 1f, .6f),
             new Color(1f, 1f, 1f, .7f),
             new Color(1f, 1f, 1f, .8f),
-            new Color(1f, 1f, 1f, .9f),
+            new Color(1f, 1f, 1f, .9f),  // i=10
             // group 2: light blue
             null,
+            new Color(0f, .7f, 1f, .1f),
             new Color(0f, .7f, 1f, .2f),
             new Color(0f, .7f, 1f, .4f),
-            new Color(0f, .7f, 1f, .8f),
+            new Color(0f, .7f, 1f, .8f),  // i=15
             // yellow-orange-red
             null,
-            new Color(1f, 1f, 0f, .1f),  // yellow
-            new Color(1f, .5f, 0f, .2f), // orange
-            new Color(1f, 0f, 0f, .4f), // red
-            new Color(.5f, 0f, 0f, .8f), // dark red
+            new Color( 1f,   1f, 0f, .05f),
+            new Color( 1f,   1f, 0f, .1f),  // yellow
+            new Color( 1f, .75f, 0f, .15f),
+            new Color( 1f,  .5f, 0f, .2f), // orange
+            new Color( 1f, .25f, 0f, .3f),
+            new Color( 1f,   0f, 0f, .4f), // red
+            new Color( 1f,  .5f, 0f, .6f),
+            new Color(.5f,   0f, 0f, .8f), // dark red   i=24
             // red markers (for errors)
             null,
             new Color(1f, 0f, 0f, 1f), // red
             new Color(1f, 0f, 0f, 1f), // red
-            new Color(1f, 0f, 0f, 1f), // red
+            new Color(1f, 0f, 0f, 1f), // red   i=28
     };
 
     public static int getMaxOfColorGroup(colorGroupKeys groupKey) {
@@ -44,11 +49,11 @@ public class DecayCellRenderer implements iCellRenderer {
             case WHITE:
                 return 10;
             case BLUE:
-                return 14;
+                return 15;
             case FIRE:
-                return 19;
+                return 24;
             default:
-                return 23;
+                return 28;
         }
     }
 

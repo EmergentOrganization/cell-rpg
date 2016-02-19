c7a9746df0ee42e24260c77cded3bdb6e57ebb2a
7yl4r <murray.tylar@gmail.com>
2016-02-19 17:35:59 -0500

    added delay to resizing in attempt to address #93; oddly... does not fix.

--- desktop/src/io/github/emergentorganization/cellrpg/desktop/DesktopLauncher.java
+++ desktop/src/io/github/emergentorganization/cellrpg/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import io.github.emergentorganization.cellrpg.tools.GameSettings;
 import io.github.emergentorganization.emergent2dcore.PixelonTransmission;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		System.out.println("intializing configuration...");
@@ -18,23 +21,33 @@ public class DesktopLauncher {
 		System.out.println("initializing application...");
 		new LwjglApplication(new PixelonTransmission(), config);
 
-		System.out.println("loading prefs...");
-		Preferences prefs = GameSettings.getPreferences();
+		final Timer timer = new Timer();
+		timer.schedule(new ResizeGraphicsTask(), 5000);
+		System.out.println("connecting to interdimentional network...");
 
-//		// uncomment and use this to set your preferred config for now: (TODO: graphics settings menu)
-//		prefs.putInteger(GameSettings.KEY_GRAPHICS_WIDTH, 1300);
-//		prefs.putInteger(GameSettings.KEY_GRAPHICS_HEIGHT, 400);
-//		prefs.flush();
-		System.out.println("getting screen prefs...");
-		int w = prefs.getInteger(GameSettings.KEY_GRAPHICS_WIDTH, LwjglApplicationConfiguration.getDesktopDisplayMode().width);
-		int h = prefs.getInteger(GameSettings.KEY_GRAPHICS_HEIGHT, LwjglApplicationConfiguration.getDesktopDisplayMode().height);
-		boolean fs = prefs.getBoolean(GameSettings.KEY_GRAPHICS_FULLSCREEN, false);
+	}
 
-		System.out.println("resizing screen to " + w + "x" + h + ", fullscreen=" + fs);
-		Gdx.graphics.setDisplayMode(
-				w,
-				h,
-				fs // fullscreen kills performance? :( why???
-		 );
+	static class ResizeGraphicsTask extends TimerTask{
+		@Override public void run(){
+
+			System.out.println("loading prefs...");
+			Preferences prefs = GameSettings.getPreferences();
+
+			//		// uncomment and use this to set your preferred config for now: (TODO: graphics settings menu)
+			//		prefs.putInteger(GameSettings.KEY_GRAPHICS_WIDTH, 1300);
+			//		prefs.putInteger(GameSettings.KEY_GRAPHICS_HEIGHT, 400);
+			//		prefs.flush();
+			System.out.println("getting screen prefs...");
+			int w = prefs.getInteger(GameSettings.KEY_GRAPHICS_WIDTH, LwjglApplicationConfiguration.getDesktopDisplayMode().width);
+			int h = prefs.getInteger(GameSettings.KEY_GRAPHICS_HEIGHT, LwjglApplicationConfiguration.getDesktopDisplayMode().height);
+			boolean fs = prefs.getBoolean(GameSettings.KEY_GRAPHICS_FULLSCREEN, false);
+
+			System.out.println("resizing screen to " + w + "x" + h + ", fullscreen=" + fs);
+			Gdx.graphics.setDisplayMode(
+					w,
+					h,
+					fs // fullscreen kills performance? :( why???
+			);
+		}
 	}
 }

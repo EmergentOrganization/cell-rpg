343b585f1f54c6954aa87280d597e9a08149e9e2
Orel Bitton <orelbitton@outlook.com>
2016-02-20 23:17:28 +0200

    fixed window freeze. window does not resize everytime

--- desktop/src/io/github/emergentorganization/cellrpg/desktop/DesktopLauncher.java
+++ desktop/src/io/github/emergentorganization/cellrpg/desktop/DesktopLauncher.java
@@ -1,6 +1,8 @@
 package io.github.emergentorganization.cellrpg.desktop;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
@@ -18,23 +20,19 @@ public class DesktopLauncher {
 		System.out.println("initializing application...");
 		new LwjglApplication(new PixelonTransmission(), config);
 
-		System.out.println("loading prefs...");
 		Preferences prefs = GameSettings.getPreferences();
 
-//		// uncomment and use this to set your preferred config for now: (TODO: graphics settings menu)
-//		prefs.putInteger(GameSettings.KEY_GRAPHICS_WIDTH, 1300);
-//		prefs.putInteger(GameSettings.KEY_GRAPHICS_HEIGHT, 400);
-//		prefs.flush();
-		System.out.println("getting screen prefs...");
-		int w = prefs.getInteger(GameSettings.KEY_GRAPHICS_WIDTH, LwjglApplicationConfiguration.getDesktopDisplayMode().width);
-		int h = prefs.getInteger(GameSettings.KEY_GRAPHICS_HEIGHT, LwjglApplicationConfiguration.getDesktopDisplayMode().height);
+		// Graphics.DisplayMode desktop = LwjglApplicationConfiguration.getDesktopDisplayMode();
+		int ww = prefs.getInteger(GameSettings.KEY_GRAPHICS_WIDTH, 1024);
+		int wh = prefs.getInteger(GameSettings.KEY_GRAPHICS_HEIGHT, 768);
 		boolean fs = prefs.getBoolean(GameSettings.KEY_GRAPHICS_FULLSCREEN, false);
 
-		System.out.println("resizing screen to " + w + "x" + h + ", fullscreen=" + fs);
+		System.out.println("Resizing: " + ww +", " + wh + ". Fullscreen: " + fs);
+
 		Gdx.graphics.setDisplayMode(
-				w,
-				h,
+				ww,
+				wh,
 				fs // fullscreen kills performance? :( why???
-		 );
+		);
 	}
 }
